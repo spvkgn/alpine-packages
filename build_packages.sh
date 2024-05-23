bad395a763fefe74a2f05a8e20fd2a49d0a3f814
spvkgn <spvkgn@users.noreply.github.com>
2024-05-23 20:24:43 +0500

    use jirutka/setup-alpine action

---
+++ build_packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euxo
+
+export PACKAGER="John Doe"
+export PACKAGER_PRIVKEY="$HOME/$GITHUB_REPOSITORY_OWNER.rsa"
+export REPODEST="$GITHUB_WORKSPACE/packages"
+
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+
+echo "$RSA_PRIVATE_KEY" > "$HOME/$GITHUB_REPOSITORY_OWNER.rsa"
+echo "$RSA_PUBLIC_KEY" > "$HOME/$GITHUB_REPOSITORY_OWNER.rsa.pub"
+cp "$HOME/$GITHUB_REPOSITORY_OWNER.rsa.pub" /etc/apk/keys
+
+mkdir -p "$REPODEST"
+# shellcheck disable=SC2016
+for dir in $GITHUB_WORKSPACE; do
+  find "$dir" -type f -name APKBUILD -exec /bin/bash -c 'cd $(dirname {}); abuild -F checksum; abuild -F -r' \;
+done
+cp "$HOME/$GITHUB_REPOSITORY_OWNER.rsa.pub" "$REPODEST"
