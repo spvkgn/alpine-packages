24253b3fa419146c7717324405196a66c8e02f1d
spvkgn <spvkgn@users.noreply.github.com>
2024-05-24 13:48:57 +0500

    add gh-pages job

--- build_packages.sh
+++ build_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo
 
-export PACKAGER="John Doe"
+export PACKAGER="Pavel <spvkgn@users.noreply.github.com>"
 export PACKAGER_PRIVKEY="$HOME/$GITHUB_REPOSITORY_OWNER.rsa"
 export REPODEST="$GITHUB_WORKSPACE/packages"
 export USE_CCACHE=1
@@ -16,8 +16,9 @@ cp "$HOME/$GITHUB_REPOSITORY_OWNER.rsa.pub" /etc/apk/keys
 mkdir -p "$REPODEST"
 # shellcheck disable=SC2016
 for dir in $GITHUB_WORKSPACE; do
-  find "$dir" -type f -name APKBUILD -exec /bin/bash -c 'cd $(dirname {}); abuild -F checksum; abuild -F -r' \;
+  find "$dir" -type f -name APKBUILD -exec /bin/bash -c 'cd $(dirname {}); abuild -F checksum; abuild -F -r -D alpine' \;
 done
 cp "$HOME/$GITHUB_REPOSITORY_OWNER.rsa.pub" "$REPODEST"
+tar cvf apk-$BRANCH-$ARCH.tar --transform "s|^\./|alpine/$BRANCH/|" --show-transformed-names -C "$REPODEST"/*/ .
 
 ccache --max-size=50M --show-stats
